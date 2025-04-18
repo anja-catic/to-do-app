1efaef15016d489d9b66cb4f0e742af01312c53c
Anja Ćatić <anja.catic@gmail.com>
2025-04-18 20:31:28 +0200

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import API_BASE_URL from "../services/api"; 
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
