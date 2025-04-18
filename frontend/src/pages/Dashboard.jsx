1361323711466cc28b3678b9a4eb4372f1ee20b5
Anja Ćatić <anja.catic@gmail.com>
2025-04-18 20:23:53 +0200

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import API_BASE_URL from "../services/api"; 
 import TaskCard from "../components/TaskCard";
 import TaskForm from "../components/TaskForm";
 import CalendarView from "../components/CalendarView";
@@ -29,7 +29,7 @@ const Dashboard = () => {
   const fetchTasks = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.get("http://127.0.0.1:8000/tasks", {
+      const response = await axios.get("${API_BASE_URL}/tasks", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
